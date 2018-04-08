3d54ea06ca43f085dc23bc1adfc5f28cc72e2497
abdou-gh549 <abdellah.abdou21@gmail.com>
2018-04-09 00:03:05 +0100

    fix Pi problem

--- app/src/main/java/com/tixx/calculatrice/util/ExpressionChek.java
+++ app/src/main/java/com/tixx/calculatrice/util/ExpressionChek.java
@@ -7,15 +7,15 @@ package com.tixx.calculatrice.util;
 public class ExpressionChek {
 
     static public boolean isOperator(String str) {
-        return !str.matches("[0123456789)]");
+        return !str.matches("[0123456789π)]");
     }
 
     static public boolean isNumber(String str) {
-        return str.matches("[0123456789]");
+        return str.matches("[0123456789π]");
     }
 
     static public boolean isNumber(char ch) {
         String str = String.valueOf(ch);
-        return str.matches("[0123456789]");
+        return str.matches("[0123456789π]");
     }
 }
